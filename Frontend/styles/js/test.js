5a67dea433eb8c6350ba92a5b025b342ec549e37
uanka <ankazeml97@gmail.com>
2016-04-11 21:35:26 +0300

    Init

---
+++ Frontend/styles/js/test.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Паша Рощина on 11.04.2016.
+ */
+
+$(document).ready(function(){
+   var doc_w = $(document).width();
+  var doc_h = $(document).height();
+    //alert(doc_w);
+    });
